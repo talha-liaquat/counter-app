18dfb94edd234ed568ccf5b8fed034ec3f1a7379
Talha Liaquat <talhaliaquat@gmail.com>
2019-04-21 16:55:24 +0400

    Single source of information

--- counter-app/src/components/counter.jsx
+++ counter-app/src/components/counter.jsx
@@ -1,34 +1,49 @@
 import React, { Component } from "react";
 
 class Counter extends Component {
-  state = {
-    count: 1,
-    tags: ["tag1", "tag2", "tag3"]
+  handleDecrement = product => {
+    this.setState({ value: this.state.value > 0 ? this.state.value - 1 : 0 });
   };
 
   render() {
     return (
-      <React.Fragment>
+      <div>
+        <span>{this.props.children}</span>
         <span className={this.getBatchClasses()}>{this.formatCount()}</span>
-        <button className="btn btn-secondary btn-sm">Increment</button>
-        <ul>
+        <button
+          onClick={() => this.props.onIncrement(this.props.counter)}
+          className="btn btn-secondary btn-sm m-2"
+        >
+          Increment
+        </button>
+        <button
+          onClick={() => this.props.onDelete(this.props.counter.id)}
+          className="btn btn-danger btn-sm m-2"
+        >
+          Delete
+        </button>
+        {/* <ul>
           {this.state.tags.map(tag => (
             <li key={tag}>{tag}</li>
           ))}
-        </ul>
-      </React.Fragment>
+        </ul> */}
+      </div>
     );
   }
 
   getBatchClasses() {
     let classes = "badge m-2 badge-";
-    classes += this.state.count === 0 ? "warning" : "primary";
+    classes += this.props.counter.value === 0 ? "warning" : "primary";
     return classes;
   }
 
   formatCount() {
-    const { count } = this.state;
+    const { value: count } = this.props.counter;
     return count === 0 ? "Zero" : count;
+  }
+
+  hide() {
+    return "display: none";
   }
 }
 
